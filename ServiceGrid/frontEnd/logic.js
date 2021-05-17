3493bfc31a445b5a88681092bcf956e12b3ab94f
NaiboWang-Alienware <naibowang@u.nus.edu>
2021-05-17 17:21:28 +0800

    Update BackEndAddress

--- ServiceGrid/frontEnd/logic.js
+++ ServiceGrid/frontEnd/logic.js
@@ -197,7 +197,7 @@ function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
     var r = window.location.search.substr(1).match(reg); //匹配目标参数
     if (r != null) return unescape(r[2]);
-    return "http://183.129.170.180:8041";
+    return "";
 }
 
 var sId = getUrlParam('id');
